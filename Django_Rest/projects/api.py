fbe7400f322d18389dd5283088a3afa333f7d985
German <gownman2004@gmail.com>
2025-07-28 21:44:32 -0400

    inicio de app task de django con react

--- Django_Rest/projects/api.py
+++
@@ -1,9 +0,0 @@
-from .models import Project
-from rest_framework import viewsets, permissions
-from .serializers import ProjectSerializer
-
-
-class ProjectViewSet(viewsets.ModelViewSet):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [permissions.AllowAny]
