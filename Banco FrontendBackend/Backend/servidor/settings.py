d45d906c370ae90f2ce520a40fb2736fea1a3f9a
German <gownman2004@gmail.com>
2025-09-01 17:07:56 -0400

    arreglo de cors allow origin

--- Banco FrontendBackend/Backend/servidor/settings.py
+++ Banco FrontendBackend/Backend/servidor/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS para permitir solicitudes desde el frontend
+CORS_ALLOW_ORIGINS = [
+    "http://localhost:3000",
+    "https://your-frontend-domain.com",
+]
